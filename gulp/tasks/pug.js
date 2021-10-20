db43f3e059e559fdfa221440e7ca5433a5afd3d0
SmValeriya <lerok1000@gmail.com>
2021-10-20 17:56:10 +0300

    Add pretty for pug on dev

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -2,12 +2,14 @@ const {src, dest} = require('gulp');
 const plumber = require('gulp-plumber');
 const pug = require('gulp-pug');
 const data = require('../../content.json');
+const env = process.env.NODE_ENV;
 
 exports.pug = () => {
   return src('./src/pages/*.pug')
     .pipe(plumber())
     .pipe(pug({
-      data: data
+      data: data,
+      pretty: env === 'dev'
     }))
     .pipe(plumber.stop())
     .pipe(dest('./build'));
