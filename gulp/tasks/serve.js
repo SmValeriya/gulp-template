946ec7dfa2c79c2fb41c5a66e6cba4257be3305d
SmValeriya <lerok1000@gmail.com>
2021-10-21 21:09:53 +0300

    Add browser-sync for reloading browser

---
+++ gulp/tasks/serve.js
@@ -0,0 +1,22 @@
+const {watch, series} = require('gulp');
+const browserSync = require('browser-sync').create();
+
+exports.serve = (done) => {
+  browserSync.init({
+    server: {
+      baseDir: "./build"
+    },
+    open: false,
+    notify: false,
+    cors: true
+  });
+  watch('./src/pages/**/*.pug', series('pug', reload));
+  watch('./src/assets/styles/**/*.pcss', series('styles', reload));
+
+  return done();
+};
+
+function reload(done) {
+  browserSync.reload();
+  done();
+}
--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@ const {series,parallel} = require('gulp');
 const {clean} = require('./gulp/tasks/clean');
 const {pug} = require('./gulp/tasks/pug');
 const {styles} = require('./gulp/tasks/styles');
+const {serve} = require('./gulp/tasks/serve');
 
-exports.html = series(pug);
+exports.pug = series(pug);
 exports.styles = series(styles);
 exports.clean = series(clean);
+exports.serve = series(serve);
 
 exports.default = series(
   clean,
-  parallel(pug, styles)
+  parallel(pug, styles),
+  serve
 );
 
 exports.build = series(
